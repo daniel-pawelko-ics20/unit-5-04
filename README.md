e2cb29c4868cd308f9ba308f7f83f77dea84f8c8
danielpawelkoic <>
2021-05-27 02:39:40 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # unit-5-04
+
+[![GitHub's Super Linter](https://github.com/daniel-pawelko-ics20/unit-5-04/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/daniel-pawelko-ics20/unit-5-04/actions)
+
+
+
+This website can be found at: [https://daniel-pawelko-ics20.github.io/unit-5-04](https://daniel-pawelko-ics20.github.io/unit-5-04)
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2021 Daniel Pawelko All rights reserved
+//
+// Created by: Daniel Pawelko
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+
+// Defining function that actives when user presses "Check" Button
+function userInputClick() {
+  // Get input from user and store it in variable
+  const age = parseInt(document.getElementById("age-entered").value)
+  const day = document.getElementById("day-entered").value
+
+  // Checks if user should go to the movies
+  if ((day == "tuesday" || day == "thursday") || (age > 12 && age < 21)) {
+    document.getElementById("output").innerHTML = "You should go because you will get a discount"
+  } else {
+    document.getElementById("output").innerHTML = "You should not go today because you will not get a discount"
+  }
+}
